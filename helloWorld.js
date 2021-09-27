12e83ed092ea5f9394a86bff54b3a15e281a7d74
rkponnam1979 <54012281+rkponnam1979@users.noreply.github.com>
2021-09-27 15:31:39 -0400

    Delete helloWorld.js

--- helloWorld.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-export default class HelloWorld extends LightningElement {
-greeting='Guru';
-changeHandler(event) {
-    this.greeting=event.target.value;
-}
-}
