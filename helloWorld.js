93458a0003577fc76ff38b9694a403d68873726d
rkponnam1979 <54012281+rkponnam1979@users.noreply.github.com>
2021-09-27 16:03:40 -0400

    Add files via upload

--- helloWorld.js
+++ helloWorld.js
@@ -1,6 +1,6 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
-greeting='Guru';
+greeting='World';
 changeHandler(event) {
     this.greeting=event.target.value;
 }
