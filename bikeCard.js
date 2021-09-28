d73b0cd494a685f007bccc430b68a91fad0ed982
rkponnam1979 <54012281+rkponnam1979@users.noreply.github.com>
2021-09-27 22:59:30 -0400

    Add files via upload

---
+++ bikeCard.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+export default class BikeCard extends LightningElement {
+    name= 'Electra X4';
+    description='A seet bike built for comfort.';
+    category='Mountain';
+    price='$2,700';
+    pictureURL='https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
+}
